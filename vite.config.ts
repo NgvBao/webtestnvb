922ce513c5a4f08261e3a8d3a4b61235a0b56fac
cuongdtdg <cuong241632016@lms.utc.edu.vn>
2025-09-18 11:53:46 +0700

    refactor: Remove API proxy configuration from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://fastapi-turbine-62vm.onrender.com",
-        changeOrigin: true,
-        secure: true, // backend dùng HTTPS (Render) -> giữ true
-        rewrite: (p) => p.replace(/^\/api/, "/api/v1"),
-      },
-    },
-  },
 });
